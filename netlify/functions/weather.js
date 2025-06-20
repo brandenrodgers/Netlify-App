8af45fd328106daba7d0eddf9a353aa1ffa47168
Branden Rodgers <brodgers@hubspot.com>
2025-06-20 09:46:18 -0400

    remove import

--- netlify/functions/weather.js
+++ netlify/functions/weather.js
@@ -1,13 +1,10 @@
 exports.handler = async (event, context) => {
   if (event.httpMethod === "GET") {
     try {
-      // Process the GET request as needed
-      const data = require("./db.json");
-
       // Return the data as the response
       return {
         statusCode: 200,
-        body: JSON.stringify(data),
+        body: JSON.stringify({ weather: "sunny" }),
       };
     } catch (error) {
       // Return an error response if there was an issue processing the request
