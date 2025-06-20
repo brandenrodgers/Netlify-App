009ed4422e46094240ff28083e64320698ef2957
Branden Rodgers <brodgers@hubspot.com>
2025-06-20 15:49:50 -0400

    fix weather response

--- netlify/functions/weather.js
+++ netlify/functions/weather.js
@@ -1,10 +1,15 @@
 exports.handler = async (event, context) => {
   if (event.httpMethod === "GET") {
     try {
+      // Extract location from query parameters
+      const location = event.queryStringParameters?.location || "Unknown";
+
       // Return the data as the response
+      const response = `The weather in ${location} is sunny`;
+
       return {
         statusCode: 200,
-        body: JSON.stringify({ weather: "sunny" }),
+        body: JSON.stringify({ weather: response }),
       };
     } catch (error) {
       // Return an error response if there was an issue processing the request
